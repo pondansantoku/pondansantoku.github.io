b9f91ed3166e3cdb04c794e0b3f05cd4cb0a6297
yosuke <yosuke9805@gmail.com>
2016-02-27 02:08:46 +0900

    shuusei

--- js/pokapoka-through.js
+++ js/pokapoka-through.js
@@ -1,7 +1,19 @@
+marked.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    langPrefix: '',
+    // highlight: function(code, lang) {
+    //     // hogehoge
+    //     return code;
+    // }
+});
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    marked.setOptions({
-        langPrefix: ''
-    });
     var innerDom = $('.post-body.entry-content');
     var markedHtml = marked(innerDom.innerHTML);
     innerDom.innerHTML = markedHtml;
